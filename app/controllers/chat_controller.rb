eca5cdc1ff58bfebab28b1e18b82329d5fb924d6
kenhakucho <bagle0309@gmail>
2016-06-21 10:37:07 +0000

    test

--- app/controllers/chat_controller.rb
+++ app/controllers/chat_controller.rb
@@ -1,21 +1,20 @@
 class ChatController < WebsocketRails::BaseController
   around_action :output_log
   
-	def initialize_session
-    	puts "### Session Initialized\n"
-  end
-	
+#  def initialize_session
+    # controller_store[:message_count] = 0
+#    puts "aaaaaaaaaaaaaa"
+#  end
+
   def new_message
-  	  puts "### call new_message: #{message}"
-	    broadcast_message :new_message, message
-	end
+    recieve_message = message()
+    broadcast_message(:new_message, recieve_message)
+  end
 
 private
-
   def output_log 
     puts "### start action"
     yield
     puts "### end action"
   end
-  
 end
